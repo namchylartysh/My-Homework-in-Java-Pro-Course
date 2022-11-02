03ef45fa43ef8201a632769858e0fd107efce54d
namchylartysh <artyshnam@gmail.com>
2022-11-03 00:04:22 +0600

    My first homework
    Added my first docs

---
+++ Cat.java
@@ -0,0 +1,26 @@
+package JavaProCourse;
+
+public class Cat {
+    private String name;
+    private String color;
+    private int age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public void voice() {
+        System.out.println("meow!");
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ HelloCat.java
@@ -0,0 +1,10 @@
+package JavaProCourse;
+
+public class HelloCat {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik", "red", 4);
+        cat.voice();
+
+        System.out.println(cat);
+    }
+}
