4791853290edb06534af71f28eb073f8a21eb09c
namchylartysh <artyshnam@gmail.com>
2022-11-10 22:32:12 +0600

    Update Zoo.java

--- homework3/Zoo.java
+++ homework3/Zoo.java
@@ -1,5 +1,12 @@
 package homework3;
 
+/**
+ * Java Professional Homework #3
+ *
+ * @author Artysh
+ * @version 07.11 - 10.11
+ */
+
 public class Zoo {
     public static void main(String[] args) {
         Cat cat = new Cat(100, 5);
