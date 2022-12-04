52d6fcdabeae44c31df6fc87101db203ef487260
namchylartysh <artyshnam@gmail.com>
2022-12-04 23:11:38 +0600

    added head to homework10

--- homework10/Lesson.java
+++ homework10/Lesson.java
@@ -1,5 +1,12 @@
 package homework10;
 
+/**
+ * Java Professional Homework #10
+ *
+ * @author Artysh
+ * @version 01.12 - 05.12
+ */
+
 public class Lesson {
     public static void main(String[] args) {
         new CommandLine().exec();
